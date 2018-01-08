0e211d3558f47c8abf84f46cdf3c60721f9dcd25
ranjan-rajeev <rajeev15069@gmail.com>
2018-01-08 15:13:14 +0530

    register changes

--- app/src/main/java/com/datacomp/magicfinmart/register/RegisterActivity.java
+++ app/src/main/java/com/datacomp/magicfinmart/register/RegisterActivity.java
@@ -1,15 +1,18 @@
 package com.datacomp.magicfinmart.register;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.datacomp.magicfinmart.BaseActivity;
 import com.datacomp.magicfinmart.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends BaseActivity implements View.OnClickListener {
+
+    LinearLayout llPersonalInfo, llProfessionalInfo;
+    ImageView ivProfessionalInfo, ivPersonalInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,42 @@ public class RegisterActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initWidgets();
+        setListener();
     }
 
+    private void setListener() {
+        ivProfessionalInfo.setOnClickListener(this);
+        ivPersonalInfo.setOnClickListener(this);
+    }
+
+    private void initWidgets() {
+        ivProfessionalInfo = (ImageView) findViewById(R.id.ivProfessionalInfo);
+        ivPersonalInfo = (ImageView) findViewById(R.id.ivPersonalInfo);
+        llPersonalInfo = (LinearLayout) findViewById(R.id.llPersonalInfo);
+        llProfessionalInfo = (LinearLayout) findViewById(R.id.llProfessionalInfo);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ivPersonalInfo:
+                hideAllLayouts(llPersonalInfo, ivPersonalInfo);
+                break;
+            case R.id.ivProfessionalInfo:
+                break;
+        }
+    }
+
+    private void hideAllLayouts(LinearLayout linearLayout, ImageView imageView) {
+
+        if (llPersonalInfo.getVisibility() == View.GONE) {
+
+            this.ivPersonalInfo.setImageDrawable(getResources().getDrawable(R.drawable.ic_add));
+            this.llPersonalInfo.setVisibility(View.GONE);
+
+            ivProfessionalInfo.setImageDrawable(getResources().getDrawable(R.drawable.ic_add));
+            llProfessionalInfo.setVisibility(View.GONE);
+        }
+    }
 }
