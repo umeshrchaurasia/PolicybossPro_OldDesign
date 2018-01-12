c36c47d1f44f2254fda1483063a4af2ff50aff74
ranjan-rajeev <rajeev15069@gmail.com>
2018-01-12 13:13:42 +0530

    changes

--- app/src/main/java/com/datacomp/magicfinmart/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/com/datacomp/magicfinmart/splashscreen/SplashScreenActivity.java
@@ -3,6 +3,7 @@ package com.datacomp.magicfinmart.splashscreen;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import com.datacomp.magicfinmart.BaseActivity;
 import com.datacomp.magicfinmart.R;
@@ -10,7 +11,14 @@ import com.datacomp.magicfinmart.introslider.PrefManager;
 import com.datacomp.magicfinmart.introslider.WelcomeActivity;
 import com.datacomp.magicfinmart.login.LoginActivity;
 
-public class SplashScreenActivity extends BaseActivity {
+import magicfinmart.datacomp.com.finmartserviceapi.master.APIResponse;
+import magicfinmart.datacomp.com.finmartserviceapi.master.IResponseSubcriber;
+import magicfinmart.datacomp.com.finmartserviceapi.master.controller.MasterController;
+import magicfinmart.datacomp.com.finmartserviceapi.master.response.AllCityMasterResponse;
+import magicfinmart.datacomp.com.finmartserviceapi.master.response.BikeMasterResponse;
+import magicfinmart.datacomp.com.finmartserviceapi.master.response.CarMasterResponse;
+
+public class SplashScreenActivity extends BaseActivity implements IResponseSubcriber {
 
     private static final String TAG = "Splashscreen";
     private final int SPLASH_DISPLAY_LENGTH = 3000;
@@ -21,6 +29,14 @@ public class SplashScreenActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         prefManager = new PrefManager(this);
+
+        if (prefManager.IsBikeMasterUpdate())
+            new MasterController(this).getBikeMaster(this);
+        if (prefManager.IsCarMasterUpdate())
+            new MasterController(this).getCarMaster(this);
+        if (prefManager.IsRtoMasterUpdate())
+            new MasterController(this).getRTOMaster(this);
+
         if (prefManager.isFirstTimeLaunch()) {
             prefManager.setFirstTimeLaunch(false);
             startActivity(new Intent(this, WelcomeActivity.class));
@@ -34,5 +50,24 @@ public class SplashScreenActivity extends BaseActivity {
         }
 
 
+    }
+
+    @Override
+    public void OnSuccess(APIResponse response, String message) {
+        if (response instanceof BikeMasterResponse) {
+            if (response.getStatusNo() == 0)
+                prefManager.setIsBikeMasterUpdate(false);
+        } else if (response instanceof CarMasterResponse) {
+            if (response.getStatusNo() == 0)
+                prefManager.setIsCarMasterUpdate(false);
+        } else if (response instanceof AllCityMasterResponse) {
+            if (response.getStatusNo() == 0)
+                prefManager.setIsRtoMasterUpdate(false);
+        }
+    }
+
+    @Override
+    public void OnFailure(Throwable t) {
+        Toast.makeText(this, "" + t.getMessage(), Toast.LENGTH_SHORT).show();
     }
 }
