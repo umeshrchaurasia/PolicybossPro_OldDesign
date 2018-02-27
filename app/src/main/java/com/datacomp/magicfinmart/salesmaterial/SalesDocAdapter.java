d534c55c07c693690109ff4b448f841265ece401
ranjan-rajeev <rajeev15069@gmail.com>
2018-02-27 11:09:55 +0530

    jcgs

--- app/src/main/java/com/datacomp/magicfinmart/salesmaterial/SalesDocAdapter.java
+++ app/src/main/java/com/datacomp/magicfinmart/salesmaterial/SalesDocAdapter.java
@@ -5,18 +5,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.datacomp.magicfinmart.R;
 import com.datacomp.magicfinmart.design.CustomImageView;
-import com.datacomp.magicfinmart.design.SquareImageView;
 
 import java.util.List;
 
-import magicfinmart.datacomp.com.finmartserviceapi.finmart.model.CompanyEntity;
 import magicfinmart.datacomp.com.finmartserviceapi.finmart.model.DocsEntity;
 
 /**
