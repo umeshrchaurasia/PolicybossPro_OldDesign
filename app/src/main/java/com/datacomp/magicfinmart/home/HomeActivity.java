4208f53eb0aaf5392e9194e46a30fa84b277b18e
NileshBirhade <nileshbirhade@gmail.com>
2018-03-01 11:52:57 +0530

    cc list changes info display

--- app/src/main/java/com/datacomp/magicfinmart/home/HomeActivity.java
+++ app/src/main/java/com/datacomp/magicfinmart/home/HomeActivity.java
@@ -20,7 +20,6 @@ import com.datacomp.magicfinmart.R;
 import com.datacomp.magicfinmart.dashboard.DashboardFragment;
 import com.datacomp.magicfinmart.helpfeedback.HelpFeedBackActivity;
 import com.datacomp.magicfinmart.loan_fm.homeloan.application.HomeLoanApplicationActivity;
-import com.datacomp.magicfinmart.loan_fm.homeloan.loan_apply.HomeLoanApplyActivity;
 import com.datacomp.magicfinmart.login.LoginActivity;
 import com.datacomp.magicfinmart.myaccount.MyAccountActivity;
 import com.datacomp.magicfinmart.notification.NotificationActivity;
@@ -29,9 +28,9 @@ import com.datacomp.magicfinmart.underconstruction.UnderConstructionActivity;
 import com.datacomp.magicfinmart.whatsnew.WhatsNewActivity;
 import com.datacomp.magicfinmart.utility.Constants;
 
+import java.util.Date;
 import java.util.List;
 
-import magicfinmart.datacomp.com.finmartserviceapi.Utility;
 import magicfinmart.datacomp.com.finmartserviceapi.database.DBPersistanceController;
 
 public class HomeActivity extends BaseActivity {
@@ -61,6 +60,7 @@ public class HomeActivity extends BaseActivity {
 
         db = new DBPersistanceController(this);
         List<String> rtoDesc = db.getRTOListNames();
+
 
         // set first fragement selected.
         navigationView.getMenu().getItem(0).setChecked(true);
@@ -94,7 +94,7 @@ public class HomeActivity extends BaseActivity {
                     // For rest of the options we just show a toast on click .
                     case R.id.nav_myaccount: {
                         startActivity(new Intent(HomeActivity.this, MyAccountActivity.class));
-                       // startActivity(new Intent(HomeActivity.this, HomeLoanApplyActivity.class));
+                        // startActivity(new Intent(HomeActivity.this, HomeLoanApplyActivity.class));
                         // fragment = new BasFragment();
                         // getSupportActionBar().setTitle("BAS 2016-17");
                         // Toast.makeText(HomeActivity.this, "my_account", Toast.LENGTH_SHORT).show();
@@ -229,7 +229,6 @@ public class HomeActivity extends BaseActivity {
         return true;
 
     }
-
 
 
     @Override
