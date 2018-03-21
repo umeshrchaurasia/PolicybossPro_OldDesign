13341ba2b79f7f9ee2f7d75bd62cfa8d981d82df
NileshBirhade <nileshbirhade@gmail.com>
2018-03-21 12:29:34 +0530

    formal commit

--- app/src/main/java/com/datacomp/magicfinmart/BaseActivity.java
+++ app/src/main/java/com/datacomp/magicfinmart/BaseActivity.java
@@ -15,14 +15,19 @@ import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -57,6 +62,10 @@ public class BaseActivity extends AppCompatActivity {
         realm = Realm.getDefaultInstance();
     }
 
+
+
+
+    //region all neccessary functions
     @Override
     protected void onResume() {
         super.onResume();
@@ -79,7 +88,7 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     protected void showDialog(String msg) {
-            dialog = ProgressDialog.show(BaseActivity.this, "", msg, true);
+        dialog = ProgressDialog.show(BaseActivity.this, "", msg, true);
     }
 
     public void sendSms(String mobNumber) {
@@ -456,5 +465,7 @@ public class BaseActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+    //endregion
 }
 
