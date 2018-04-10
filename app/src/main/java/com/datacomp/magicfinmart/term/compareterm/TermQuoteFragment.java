93c43f206b4d398b6990d5b1a30eb38681e9b5cc
ranjan-rajeev <rajeev15069@gmail.com>
2018-04-10 15:46:28 +0530

    term insurance

--- app/src/main/java/com/datacomp/magicfinmart/term/compareterm/TermQuoteFragment.java
+++ app/src/main/java/com/datacomp/magicfinmart/term/compareterm/TermQuoteFragment.java
@@ -1,19 +1,27 @@
 package com.datacomp.magicfinmart.term.compareterm;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.datacomp.magicfinmart.BaseFragment;
 import com.datacomp.magicfinmart.R;
+import com.datacomp.magicfinmart.term.compareterm.adapters.TermQuoteAdapter;
+import com.datacomp.magicfinmart.webviews.CommonWebViewActivity;
 
+import magicfinmart.datacomp.com.finmartserviceapi.Utility;
 import magicfinmart.datacomp.com.finmartserviceapi.finmart.APIResponse;
 import magicfinmart.datacomp.com.finmartserviceapi.finmart.IResponseSubcriber;
 import magicfinmart.datacomp.com.finmartserviceapi.finmart.controller.term.TermInsuranceController;
+import magicfinmart.datacomp.com.finmartserviceapi.finmart.model.TermCompareResponseEntity;
 import magicfinmart.datacomp.com.finmartserviceapi.finmart.requestentity.TermFinmartRequest;
 import magicfinmart.datacomp.com.finmartserviceapi.finmart.requestentity.TermRequestEntity;
 import magicfinmart.datacomp.com.finmartserviceapi.finmart.response.TermCompareQuoteResponse;
@@ -25,7 +33,11 @@ import magicfinmart.datacomp.com.finmartserviceapi.finmart.response.TermCompareQ
 public class TermQuoteFragment extends BaseFragment implements View.OnClickListener, BaseFragment.PopUpListener, IResponseSubcriber {
     TermFinmartRequest termFinmartRequest;
     TermRequestEntity termRequestEntity;
-    TextView tvSum, tvGender, tvSmoker, tvAge, tvPolicyTerm, tvCrn;
+    TextView tvSum, tvGender, tvSmoker, tvAge, tvPolicyTerm, tvCrn, filter;
+    ImageView ivEdit;
+    TermQuoteAdapter mAdapter;
+    TermCompareQuoteResponse termCompareQuoteResponse;
+    RecyclerView rvTerm;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,16 +67,22 @@ public class TermQuoteFragment extends BaseFragment implements View.OnClickListe
     }
 
     private void initView(View view) {
-
+        rvTerm = (RecyclerView) view.findViewById(R.id.rvTerm);
+        rvTerm.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvTerm.setHasFixedSize(true);
         tvSum = (TextView) view.findViewById(R.id.tvSum);
         tvGender = (TextView) view.findViewById(R.id.tvGender);
         tvSmoker = (TextView) view.findViewById(R.id.tvSmoker);
         tvAge = (TextView) view.findViewById(R.id.tvAge);
         tvPolicyTerm = (TextView) view.findViewById(R.id.tvPolicyTerm);
         tvCrn = (TextView) view.findViewById(R.id.tvCrn);
+        ivEdit = (ImageView) view.findViewById(R.id.ivEdit);
+        filter = (TextView) view.findViewById(R.id.filter);
     }
 
     private void setListener() {
+        ivEdit.setOnClickListener(this);
+        filter.setOnClickListener(this);
     }
 
     private void bindHeaders() {
@@ -80,7 +98,7 @@ public class TermQuoteFragment extends BaseFragment implements View.OnClickListe
                 tvSmoker.setText("NON-SMOKER");
             tvAge.setText("" + termRequestEntity.getInsuredDOB());
             tvPolicyTerm.setText("" + termRequestEntity.getPolicyTerm() + " YEARS");
-            tvCrn.setText("" + termRequestEntity.getSumAssured());
+            tvCrn.setText("---");
         }
     }
 
@@ -91,7 +109,12 @@ public class TermQuoteFragment extends BaseFragment implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()) {
+            case R.id.filter:
+            case R.id.ivEdit:
+                ((CompareTermActivity) getActivity()).redirectToInput(termFinmartRequest);
+                break;
+        }
     }
 
     @Override
@@ -104,10 +127,35 @@ public class TermQuoteFragment extends BaseFragment implements View.OnClickListe
 
     }
 
+    public void redirectToBuy(TermCompareResponseEntity entity) {
+        if (Utility.checkShareStatus(getActivity()) == 1) {
+
+            //convert quote to application server
+               /* new QuoteApplicationController(getActivity()).convertQuoteToApp(
+                        "" + saveQuoteEntity.getVehicleRequestID(),
+                        entity.getInsurer_Id(), imgPath,
+                        this);*/
+
+            startActivity(new Intent(getActivity(), CommonWebViewActivity.class)
+                    .putExtra("URL", entity.getProposerPageUrl())
+                    .putExtra("NAME", "" + entity.getInsurerName())
+                    .putExtra("TITLE", "" + entity.getInsurerName()));
+
+        } else {
+            openPopUp(ivEdit, "Message", "Your POSP status is INACTIVE", "OK", true);
+        }
+
+
+    }
+
     @Override
     public void OnSuccess(APIResponse response, String message) {
         if (response instanceof TermCompareQuoteResponse) {
             cancelDialog();
+            this.termCompareQuoteResponse = (TermCompareQuoteResponse) response;
+            mAdapter = new TermQuoteAdapter(TermQuoteFragment.this, termCompareQuoteResponse);
+            rvTerm.setAdapter(mAdapter);
+            tvCrn.setText("" + termCompareQuoteResponse.getMasterData().getResponse().get(0).getCustomerReferenceID());
         }
 
     }
