c1ed8864fb925d77cbe817d8bad1608048449f50
ranjan-rajeev <rajeev15069@gmail.com>
2018-06-04 12:55:23 +0530

    date validation changes

--- app/src/main/java/com/datacomp/magicfinmart/motor/twowheeler/fragment/BikeInputFragment.java
+++ app/src/main/java/com/datacomp/magicfinmart/motor/twowheeler/fragment/BikeInputFragment.java
@@ -1305,21 +1305,20 @@ public class BikeInputFragment extends BaseFragment implements BaseFragment.PopU
 
             //region policy expirydate
             else if (view.getId() == R.id.etExpDate) {
+                Calendar calendar = Calendar.getInstance();
                 Date regDate = new Date();
                 if (etRegDate.getText().toString().isEmpty()) {
-                    Calendar calendar = Calendar.getInstance();
                     regDate = calendar.getTime();
                 } else {
                     try {
                         regDate = displayFormat.parse(etRegDate.getText().toString());
                     } catch (ParseException e) {
-                        Calendar calendar = Calendar.getInstance();
                         regDate = calendar.getTime();
                         e.printStackTrace();
                     }
                 }
 
-                DateTimePicker.policyExpValidation(view.getContext(), regDate, new DatePickerDialog.OnDateSetListener() {
+                DateTimePicker.BikepolicyExpValidation(view.getContext(), calendar, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view1, int year, int monthOfYear, int dayOfMonth) {
                         if (view1.isShown()) {
