a1ff9858c55b3d29cddaab2280930a8375c63c1e
NileshBirhade <nileshbirhade@gmail.com>
2018-08-06 17:09:32 +0530

    vehicle details added

--- finmartserviceapi/src/main/java/magicfinmart/datacomp/com/finmartserviceapi/dynamic_urls/DynamicUrlBuilder.java
+++ finmartserviceapi/src/main/java/magicfinmart/datacomp/com/finmartserviceapi/dynamic_urls/DynamicUrlBuilder.java
@@ -1,19 +1,14 @@
 package magicfinmart.datacomp.com.finmartserviceapi.dynamic_urls;
 
-import java.util.HashMap;
-
-import magicfinmart.datacomp.com.finmartserviceapi.healthcheckup.HealthCheckUPRetroRequestBuilder;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import retrofit2.http.Url;
 
 /**
  * Created by Nilesh Birhade on 06-08-2018.
  */
 
-public class DynamicUrlBuilder extends HealthCheckUPRetroRequestBuilder {
+public class DynamicUrlBuilder extends GenericRetroRequestBuilder {
 
 
     public GenericUrlNetworkService getService() {
@@ -25,7 +20,7 @@ public class DynamicUrlBuilder extends HealthCheckUPRetroRequestBuilder {
         @GET
         Call<VehicleInfoEntity> getVehicleByVehicleNo(@Url String strUrl);
 
-        @POST
-        Call<VehicleMobileResponse> getVehicleByMobNo(@Url String strUrl, @Body HashMap<String, String> body);
+        @GET
+        Call<VehicleMobileResponse> getVehicleByMobNo(@Url String strGetUrl);
     }
 }
