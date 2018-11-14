c26295abfd203282783afe527ed963a76edc73c4
Rahul <rahulrchaurasia@gmail.com>
2018-11-14 11:28:00 +0530

    Vehicle Scan Open Camera

--- app/src/main/java/com/datacomp/magicfinmart/scan_vehicle/VehicleScanActivity.java
+++ app/src/main/java/com/datacomp/magicfinmart/scan_vehicle/VehicleScanActivity.java
@@ -1,15 +1,19 @@
 package com.datacomp.magicfinmart.scan_vehicle;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,11 +25,14 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.datacomp.magicfinmart.BaseActivity;
 import com.datacomp.magicfinmart.BuildConfig;
 import com.datacomp.magicfinmart.R;
+import com.datacomp.magicfinmart.utility.Constants;
 import com.datacomp.magicfinmart.vehicle_details.VehicleDetailFragment;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.Text;
@@ -34,17 +41,36 @@ import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 
-public class VehicleScanActivity extends AppCompatActivity {
+public class VehicleScanActivity extends BaseActivity implements BaseActivity.PopUpListener {
 
     private static final String LOG_TAG = "Text API";
     private static final int PHOTO_REQUEST = 10;
     private TextView scanResults;
+    ImageView imageView;
     private Uri imageUri;
     private TextRecognizer detector;
     private static final int REQUEST_WRITE_PERMISSION = 20;
     private static final String SAVED_INSTANCE_URI = "uri";
     private static final String SAVED_INSTANCE_RESULT = "result";
+
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int requestPermissionID = 101;
+    private String PHOTO_File = "ScanPhotograph";
+    Button button;
+
+    File Docfile;
+    File file;
+    InputStream inputStream;
+    ExifInterface ei;
+
+    String[] perms = {
+            "android.permission.CAMERA",
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.READ_EXTERNAL_STORAGE"
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +80,8 @@ public class VehicleScanActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        Button button = (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
         scanResults = (TextView) findViewById(R.id.results);
         if (savedInstanceState != null) {
             imageUri = Uri.parse(savedInstanceState.getString(SAVED_INSTANCE_URI));
@@ -70,78 +96,80 @@ public class VehicleScanActivity extends AppCompatActivity {
             }
         });
 
+        CameraPopUp();
+
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_WRITE_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    takePicture();
-                } else {
-                    Toast.makeText(VehicleScanActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
-                }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PHOTO_REQUEST && resultCode == RESULT_OK) {
-            launchMediaScanIntent();
-            try {
-                Bitmap bitmap = decodeBitmapUri(this, imageUri);
-                if (detector.isOperational() && bitmap != null) {
-                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-                    SparseArray<TextBlock> textBlocks = detector.detect(frame);
-                    String blocks = "";
-                    String lines = "";
-                    String words = "";
-                    for (int index = 0; index < textBlocks.size(); index++) {
-                        //extract scanned text blocks here
-                        TextBlock tBlock = textBlocks.valueAt(index);
-                        blocks = blocks + tBlock.getValue() + "\n" + "\n";
-                        for (Text line : tBlock.getComponents()) {
-                            //extract scanned text lines here
-                            lines = lines + line.getValue() + "\n";
-                            for (Text element : line.getComponents()) {
-                                //extract scanned text words here
-                                words = words + element.getValue() + ", ";
-                            }
-                        }
-                    }
-                    if (textBlocks.size() == 0) {
-                        scanResults.setText("Scan Failed: Found nothing to scan");
-                    } else {
-                        scanResults.setText(scanResults.getText() + "Blocks: " + "\n");
-                        scanResults.setText(scanResults.getText() + blocks + "\n");
-                        scanResults.setText(scanResults.getText() + "---------" + "\n");
-                        scanResults.setText(scanResults.getText() + "Lines: " + "\n");
-                        scanResults.setText(scanResults.getText() + lines + "\n");
-                        scanResults.setText(scanResults.getText() + "---------" + "\n");
-                        scanResults.setText(scanResults.getText() + "Words: " + "\n");
-                        scanResults.setText(scanResults.getText() + words + "\n");
-                        scanResults.setText(scanResults.getText() + "---------" + "\n");
-                    }
-                } else {
-                    scanResults.setText("Could not set up the detector!");
-                }
-            } catch (Exception e) {
-                Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT)
-                        .show();
-                Log.e(LOG_TAG, e.toString());
-            }
-        }
-    }
-
-    private void takePicture() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photo = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
-        imageUri = FileProvider.getUriForFile(VehicleScanActivity.this,
-                BuildConfig.APPLICATION_ID + ".provider", photo);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-        startActivityForResult(intent, PHOTO_REQUEST);
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        switch (requestCode) {
+//            case REQUEST_WRITE_PERMISSION:
+//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    takePicture();
+//                } else {
+//                    Toast.makeText(VehicleScanActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+//                }
+//        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == PHOTO_REQUEST && resultCode == RESULT_OK) {
+//            launchMediaScanIntent();
+//            try {
+//                Bitmap bitmap = decodeBitmapUri(this, imageUri);
+//                if (detector.isOperational() && bitmap != null) {
+//                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+//                    SparseArray<TextBlock> textBlocks = detector.detect(frame);
+//                    String blocks = "";
+//                    String lines = "";
+//                    String words = "";
+//                    for (int index = 0; index < textBlocks.size(); index++) {
+//                        //extract scanned text blocks here
+//                        TextBlock tBlock = textBlocks.valueAt(index);
+//                        blocks = blocks + tBlock.getValue() + "\n" + "\n";
+//                        for (Text line : tBlock.getComponents()) {
+//                            //extract scanned text lines here
+//                            lines = lines + line.getValue() + "\n";
+//                            for (Text element : line.getComponents()) {
+//                                //extract scanned text words here
+//                                words = words + element.getValue() + ", ";
+//                            }
+//                        }
+//                    }
+//                    if (textBlocks.size() == 0) {
+//                        scanResults.setText("Scan Failed: Found nothing to scan");
+//                    } else {
+//                        scanResults.setText(scanResults.getText() + "Blocks: " + "\n");
+//                        scanResults.setText(scanResults.getText() + blocks + "\n");
+//                        scanResults.setText(scanResults.getText() + "---------" + "\n");
+//                        scanResults.setText(scanResults.getText() + "Lines: " + "\n");
+//                        scanResults.setText(scanResults.getText() + lines + "\n");
+//                        scanResults.setText(scanResults.getText() + "---------" + "\n");
+//                        scanResults.setText(scanResults.getText() + "Words: " + "\n");
+//                        scanResults.setText(scanResults.getText() + words + "\n");
+//                        scanResults.setText(scanResults.getText() + "---------" + "\n");
+//                    }
+//                } else {
+//                    scanResults.setText("Could not set up the detector!");
+//                }
+//            } catch (Exception e) {
+//                Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT)
+//                        .show();
+//                Log.e(LOG_TAG, e.toString());
+//            }
+//        }
+//    }
+//
+//    private void takePicture() {
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        File photo = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
+//        imageUri = FileProvider.getUriForFile(VehicleScanActivity.this,
+//                BuildConfig.APPLICATION_ID + ".provider", photo);
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+//        startActivityForResult(intent, PHOTO_REQUEST);
+//    }
 
     private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
         int targetW = 600;
@@ -174,4 +202,204 @@ public class VehicleScanActivity extends AppCompatActivity {
         mediaScanIntent.setData(imageUri);
         this.sendBroadcast(mediaScanIntent);
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        if (requestCode != requestPermissionID) {
+//            Log.d(TAG, "Got unexpected permission result: " + requestCode);
+//            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//            return;
+//        }
+//
+//        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//            try {
+//                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//                    return;
+//                }
+//                mCameraSource.start(mCameraView.getHolder());
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+        switch (requestCode) {
+            case Constants.PERMISSION_CAMERA_STORACGE_CONSTANT:
+                if (grantResults.length > 0) {
+
+                    //boolean writeExternal = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                    boolean camera = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeExternal = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    boolean readExternal = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+
+                    if (camera && writeExternal && readExternal) {
+
+
+                        launchCamera();
+
+                    }
+
+                }
+                break;
+        }
+    }
+
+    // region camera
+
+
+    private void CameraPopUp() {
+
+        if (!checkPermission()) {
+
+            if (checkRationalePermission()) {
+                requestPermission();
+
+            } else {
+
+                openPopUp(button, "Need  Permission", "This app needs all permissions.", "GRANT", true);
+
+
+            }
+        } else {
+
+            launchCamera();
+        }
+    }
+
+
+    private void launchCamera() {
+
+
+        String FileName = "";
+        FileName = PHOTO_File;
+
+
+        Docfile = createFile(FileName);
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            imageUri = Uri.fromFile(Docfile);
+        } else {
+            imageUri = FileProvider.getUriForFile(VehicleScanActivity.this,
+                    getString(R.string.file_provider_authority), Docfile);
+        }
+
+
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,
+                imageUri);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap bitmap = null;
+            try {
+              //  bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                 bitmap = decodeBitmapUri(this, imageUri);
+                imageView.setImageBitmap(bitmap);
+                Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show();
+
+                try {
+                   // Bitmap bitmap = decodeBitmapUri(this, imageUri);
+                    if (detector.isOperational() && bitmap != null) {
+                        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                        SparseArray<TextBlock> textBlocks = detector.detect(frame);
+                        String blocks = "";
+                        String lines = "";
+                        String words = "";
+                        for (int index = 0; index < textBlocks.size(); index++) {
+                            //extract scanned text blocks here
+                            TextBlock tBlock = textBlocks.valueAt(index);
+                            blocks = blocks + tBlock.getValue() + "\n" + "\n";
+                            for (Text line : tBlock.getComponents()) {
+                                //extract scanned text lines here
+                                lines = lines + line.getValue() + "\n";
+                                for (Text element : line.getComponents()) {
+                                    //extract scanned text words here
+                                    words = words + element.getValue() + ", ";
+                                }
+                            }
+                        }
+                        if (textBlocks.size() == 0) {
+                            scanResults.setText("Scan Failed: Found nothing to scan");
+                        } else {
+                            scanResults.setText(scanResults.getText() + "Blocks: " + "\n");
+                            scanResults.setText(scanResults.getText() + blocks + "\n");
+                            scanResults.setText(scanResults.getText() + "---------" + "\n");
+                            scanResults.setText(scanResults.getText() + "Lines: " + "\n");
+                            scanResults.setText(scanResults.getText() + lines + "\n");
+                            scanResults.setText(scanResults.getText() + "---------" + "\n");
+                            scanResults.setText(scanResults.getText() + "Words: " + "\n");
+                            scanResults.setText(scanResults.getText() + words + "\n");
+                            scanResults.setText(scanResults.getText() + "---------" + "\n");
+                        }
+                    } else {
+                        scanResults.setText("Could not set up the detector!");
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT)
+                            .show();
+                    Log.e(LOG_TAG, e.toString());
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+
+    // region permission
+    private boolean checkPermission() {
+
+        int camera = ActivityCompat.checkSelfPermission(getApplicationContext(), perms[0]);
+
+        int WRITE_EXTERNAL = ActivityCompat.checkSelfPermission(getApplicationContext(), perms[1]);
+        int READ_EXTERNAL = ActivityCompat.checkSelfPermission(getApplicationContext(), perms[2]);
+
+        return camera == PackageManager.PERMISSION_GRANTED
+                && WRITE_EXTERNAL == PackageManager.PERMISSION_GRANTED
+                && READ_EXTERNAL == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean checkRationalePermission() {
+
+        boolean camera = ActivityCompat.shouldShowRequestPermissionRationale(VehicleScanActivity.this, perms[0]);
+
+        boolean write_external = ActivityCompat.shouldShowRequestPermissionRationale(VehicleScanActivity.this, perms[1]);
+        boolean read_external = ActivityCompat.shouldShowRequestPermissionRationale(VehicleScanActivity.this, perms[2]);
+
+        return camera || write_external || read_external;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, perms, Constants.PERMISSION_CAMERA_STORACGE_CONSTANT);
+    }
+
+    @Override
+    public void onPositiveButtonClick(Dialog dialog, View view) {
+
+        dialog.cancel();
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, Constants.REQUEST_PERMISSION_SETTING);
+
+    }
+
+    @Override
+    public void onCancelButtonClick(Dialog dialog, View view) {
+
+        dialog.cancel();
+    }
+
+    //endregion
+
+    //endregion
+
 }
