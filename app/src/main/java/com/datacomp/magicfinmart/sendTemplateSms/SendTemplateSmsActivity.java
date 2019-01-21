6ce7e7d93fd09834501370883fe14b5217a0e7f4
ranjan-rajeev <rajeev15069@gmail.com>
2019-01-21 19:36:58 +0530

    UI

--- app/src/main/java/com/datacomp/magicfinmart/sendTemplateSms/SendTemplateSmsActivity.java
+++ app/src/main/java/com/datacomp/magicfinmart/sendTemplateSms/SendTemplateSmsActivity.java
@@ -1,14 +1,22 @@
 package com.datacomp.magicfinmart.sendTemplateSms;
 
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.Context;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.datacomp.magicfinmart.BaseActivity;
@@ -39,6 +47,8 @@ public class SendTemplateSmsActivity extends BaseActivity implements IResponseSu
     DBPersistanceController dbPersistanceController;
     LoginResponseEntity loginEntity;
     PrefManager prefManager;
+
+    Button btnAddContact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,7 +67,79 @@ public class SendTemplateSmsActivity extends BaseActivity implements IResponseSu
         showDialog("Fetching Data...");
         new RegisterController(SendTemplateSmsActivity.this).getSmsTemplate( SendTemplateSmsActivity.this);
 
+        btnAddContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+//                try {
+//                ArrayList<ContentProviderOperation> ops =
+//                        new ArrayList<ContentProviderOperation>();
+//
+//                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+//                        .withValue(ContactsContract.Data.RAW_CONTACT_ID, 22000)
+//                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+//                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, "9990009920")
+//                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_CUSTOM)
+//                        .withValue(ContactsContract.CommonDataKinds.Phone.LABEL, "Finmart Contact")
+//                        .build());
+//
+//                    getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+//
+//                    Toast.makeText(SendTemplateSmsActivity.this,"Contact Saved Successfully...",Toast.LENGTH_SHORT).show();
+//                } catch (RemoteException e) {
+//                    e.printStackTrace();
+//                } catch (OperationApplicationException e) {
+//                    e.printStackTrace();
+//                }
+
+
+                WritePhoneContact("Finmart Contact","9999911111",SendTemplateSmsActivity.this);
+
+            }
+        });
+
+    }
+
+    public void WritePhoneContact(String displayName, String number, Context cntx /*App or Activity Ctx*/) {
+        Context contetx = cntx; //Application's context or Activity's context
+        String strDisplayName = displayName; // Name of the Person to add
+        String strNumber = number; //number of the person to add with the Contact
+        System.out.println("NAME> " + strDisplayName + "    NUMBER ====>  " + strNumber);
+        ArrayList<ContentProviderOperation> cntProOper = new ArrayList<ContentProviderOperation>();
+        int contactIndex = cntProOper.size();//ContactSize
+
+        //Newly Inserted contact
+        // A raw contact will be inserted ContactsContract.RawContacts table in contacts database.
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)//Step1
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
+
+        //Display name will be inserted in ContactsContract.Data table
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)//Step2
+                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, contactIndex)
+                .withValue(ContactsContract.RawContacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, strDisplayName) // Name of the contact
+                .build());
+        //Mobile number will be inserted in ContactsContract.Data table
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)//Step 3
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, contactIndex)
+                .withValue(ContactsContract.RawContacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, strNumber) // Number to be added
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE).build()); //Type like HOME, MOBILE etc
+        try {
+            // We will do batch operation to insert all above data
+            //Contains the output of the app of a ContentProviderOperation.
+            //It is sure to have exactly one of uri or count set
+            ContentProviderResult[] contentProresult = null;
+            contentProresult = contetx.getContentResolver().applyBatch(ContactsContract.AUTHORITY, cntProOper); //apply above data insertion into contacts list
+
+            Toast.makeText(SendTemplateSmsActivity.this,"Contact Saved Successfully...",Toast.LENGTH_SHORT).show();
+
+        } catch (RemoteException exp) {
+            //logs;
+        } catch (OperationApplicationException exp) {
+            //logs
+        }
     }
 
     private void initialize() {
@@ -73,7 +155,7 @@ public class SendTemplateSmsActivity extends BaseActivity implements IResponseSu
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(SendTemplateSmsActivity.this);
         rvSendSms.setLayoutManager(layoutManager);
 
-
+        btnAddContact =  findViewById(R.id.btnAddContact);
 
     }
 
